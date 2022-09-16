47e13a6bba9860808068a600334ad1fb358243c9
Mohamed Hamdy Ahmed Mohamed <mohamedhamdyahmed94@gmail.com>
2022-09-16 17:02:14 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.37bdfd7da9884e74b7b00584d1f41a4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad13af3a509e8715685e",
+    "url": "/Movies-App-toolkit-saga/static/css/main.e5974737.chunk.css"
+  },
+  {
+    "revision": "ad13af3a509e8715685e",
+    "url": "/Movies-App-toolkit-saga/static/js/main.ad13af3a.chunk.js"
+  },
+  {
+    "revision": "b74c6bea94ebc2dd106c",
+    "url": "/Movies-App-toolkit-saga/static/js/1.b74c6bea.chunk.js"
+  },
+  {
+    "revision": "3f2be82d2e2ff1eab92b",
+    "url": "/Movies-App-toolkit-saga/static/css/2.f5556575.chunk.css"
+  },
+  {
+    "revision": "3f2be82d2e2ff1eab92b",
+    "url": "/Movies-App-toolkit-saga/static/js/2.3f2be82d.chunk.js"
+  },
+  {
+    "revision": "7d659aae31e99a53602b",
+    "url": "/Movies-App-toolkit-saga/static/js/runtime~main.7d659aae.js"
+  },
+  {
+    "revision": "c8030abb5fb4b9504a78ca4541fe7f66",
+    "url": "/Movies-App-toolkit-saga/index.html"
+  }
+];
